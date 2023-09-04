8d939eb54afdcb5ab1fc9bc76d6914bf108a4d36
Admin Asisst III <Admin Asisst III@DESKTOP-N0HRD0G>
2023-09-04 11:12:17 +0800

    Add project files.

---
+++ BioMetrixCore/Info/MachineInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BioMetrixCore
+{
+    public class MachineInfo
+    {
+        public int MachineNumber { get; set; }
+        public int IndRegID { get; set; }
+        public string DateTimeRecord { get; set; }
+        public int DailyRecordCount { get; set; }
+        public String DateOnlyRecord
+        {
+            get { return DateTime.Parse(DateTimeRecord).ToString("MMM/dd/yyyy"); }
+        }
+        public String TimeOnlyRecord
+        {
+            get { return DateTime.Parse(DateTimeRecord).ToString("hh:mm:ss tt"); }
+        }
+
+        public string Remarks { get; set; }
+
+    }
+}
---
+++ BioMetrixCore/Master.cs
@@ -0,0 +1,520 @@
+﻿///
+///    Experimented By : Ozesh Thapa
+///    Email: dablackscarlet@gmail.com
+///
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Linq;
+
+namespace BioMetrixCore
+{
+    public partial class Master : Form
+    {
+        DeviceManipulator manipulator = new DeviceManipulator();
+        public ZkemClient objZkeeper;
+        private bool isDeviceConnected = false;
+
+        public bool IsDeviceConnected
+        {
+            get { return isDeviceConnected; }
+            set
+            {
+                isDeviceConnected = value;
+                if (isDeviceConnected)
+                {
+                    ShowStatusBar("The device is connected !!", true);
+                    btnConnect.Text = "Disconnect";
+                    ToggleControls(true);
+                }
+                else
+                {
+                    ShowStatusBar("The device is diconnected !!", true);
+                    objZkeeper.Disconnect();
+                    btnConnect.Text = "Connect";
+                    ToggleControls(false);
+                }
+            }
+        }
+
+
+        private void ToggleControls(bool value)
+        {
+            btnBeep.Enabled = value;
+            btnDownloadFingerPrint.Enabled = value;
+            btnPullData.Enabled = value;
+            btnPowerOff.Enabled = value;
+            btnRestartDevice.Enabled = value;
+            btnGetDeviceTime.Enabled = value;
+            btnEnableDevice.Enabled = value;
+            btnDisableDevice.Enabled = value;
+            btnGetAllUserID.Enabled = value;
+            btnUploadUserInfo.Enabled = value;
+            btnSortUser.Enabled = value;
+            btnSaveData.Enabled = value;
+            tbxMachineNumber.Enabled = !value;
+            tbxPort.Enabled = !value;
+            tbxDeviceIP.Enabled = !value;
+
+        }
+
+        public Master()
+        {
+            InitializeComponent();
+            ToggleControls(false);
+            ShowStatusBar(string.Empty, true);
+            DisplayEmpty();
+        }
+
+
+        /// <summary>
+        /// Your Events will reach here if implemented
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="actionType"></param>
+        private void RaiseDeviceEvent(object sender, string actionType)
+        {
+            switch (actionType)
+            {
+                case UniversalStatic.acx_Disconnect:
+                    {
+                        ShowStatusBar("The device is switched off", true);
+                        DisplayEmpty();
+                        btnConnect.Text = "Connect";
+                        ToggleControls(false);
+                        break;
+                    }
+
+                default:
+                    break;
+            }
+
+        }
+
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                ShowStatusBar(string.Empty, true);
+
+                if (IsDeviceConnected)
+                {
+                    IsDeviceConnected = false;
+                    this.Cursor = Cursors.Default;
+
+                    return;
+                }
+
+                string ipAddress = tbxDeviceIP.Text.Trim();
+                string port = tbxPort.Text.Trim();
+                if (ipAddress == string.Empty || port == string.Empty)
+                    throw new Exception("The Device IP Address and Port is mandotory !!");
+
+                int portNumber = 4370;
+                if (!int.TryParse(port, out portNumber))
+                    throw new Exception("Not a valid port number");
+
+                bool isValidIpA = UniversalStatic.ValidateIP(ipAddress);
+                if (!isValidIpA)
+                    throw new Exception("The Device IP is invalid !!");
+
+                isValidIpA = UniversalStatic.PingTheDevice(ipAddress);
+                if (!isValidIpA)
+                    throw new Exception("The device at " + ipAddress + ":" + port + " did not respond!!");
+
+                objZkeeper = new ZkemClient(RaiseDeviceEvent);   
+                IsDeviceConnected = objZkeeper.Connect_Net(ipAddress, portNumber);
+
+                if (IsDeviceConnected)
+                {
+                    string deviceInfo = manipulator.FetchDeviceInfo(objZkeeper, int.Parse(tbxMachineNumber.Text.Trim()));
+                    lblDeviceInfo.Text = deviceInfo;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ShowStatusBar(ex.Message, false);
+            }
+            this.Cursor = Cursors.Default;
+
+        }
+
+
+        public void ShowStatusBar(string message, bool type)
+        {
+            if (message.Trim() == string.Empty)
+            {
+                lblStatus.Visible = false;
+                return;
+            }
+
+            lblStatus.Visible = true;
+            lblStatus.Text = message;
+            lblStatus.ForeColor = Color.White;
+
+            if (type)
+                lblStatus.BackColor = Color.FromArgb(79, 208, 154);
+            else
+                lblStatus.BackColor = Color.FromArgb(230, 112, 134);
+        }
+
+
+        private void btnPingDevice_Click(object sender, EventArgs e)
+        {
+            ShowStatusBar(string.Empty, true);
+
+            string ipAddress = tbxDeviceIP.Text.Trim();
+
+            bool isValidIpA = UniversalStatic.ValidateIP(ipAddress);
+            if (!isValidIpA)
+                throw new Exception("The Device IP is invalid !!");
+
+            isValidIpA = UniversalStatic.PingTheDevice(ipAddress);
+            if (isValidIpA)
+                ShowStatusBar("The device is active", true);
+            else
+                ShowStatusBar("Could not read any response", false);
+        }
+
+        private void btnGetAllUserID_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ICollection<UserIDInfo> lstUserIDInfo = manipulator.GetAllUserID(objZkeeper, int.Parse(tbxMachineNumber.Text.Trim()));
+                
+
+                if (lstUserIDInfo != null && lstUserIDInfo.Count > 0)
+                {
+                    BindToGridView(lstUserIDInfo);
+                    ShowStatusBar(lstUserIDInfo.Count + " records found !!", true);
+                }
+                else
+                {
+                    DisplayEmpty();
+                    DisplayListOutput("No records found");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                DisplayListOutput(ex.Message);
+            }
+
+        }
+
+        private void btnBeep_Click(object sender, EventArgs e)
+        {
+            objZkeeper.Beep(100);
+        }
+
+        private void btnDownloadFingerPrint_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ShowStatusBar(string.Empty, true);
+
+                ICollection<UserInfo> lstFingerPrintTemplates = manipulator.GetAllUserInfo(objZkeeper, int.Parse(tbxMachineNumber.Text.Trim()));
+                if (lstFingerPrintTemplates != null && lstFingerPrintTemplates.Count > 0)
+                {
+                    BindToGridView(lstFingerPrintTemplates);
+                    ShowStatusBar(lstFingerPrintTemplates.Count + " records found !!", true);
+                }
+                else
+                    DisplayListOutput("No records found");
+            }
+            catch (Exception ex)
+            {
+                DisplayListOutput(ex.Message);
+            }
+
+        }
+
+
+        private void btnPullData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ShowStatusBar(string.Empty, true);
+
+                ICollection<MachineInfo> lstMachineInfo = manipulator.GetLogData(objZkeeper, int.Parse(tbxMachineNumber.Text.Trim()));
+                
+                if (lstMachineInfo != null && lstMachineInfo.Count > 0)
+                {
+                    BindToGridView(lstMachineInfo.ToList());
+                    ShowStatusBar(lstMachineInfo.Count + " records found !!", true);
+                }
+                else
+                    DisplayListOutput("No records found");
+            }
+            catch (Exception ex)
+            {
+                DisplayListOutput(ex.Message);
+            }
+
+        }
+
+
+        private void ClearGrid()
+        {
+            if (dgvRecords.Controls.Count > 2)
+            { dgvRecords.Controls.RemoveAt(2); }
+
+
+            dgvRecords.DataSource = null;
+            dgvRecords.Controls.Clear();
+            dgvRecords.Rows.Clear();
+            dgvRecords.Columns.Clear();
+        }
+        private void BindToGridView(object list)
+        {
+            ClearGrid();
+            dgvRecords.DataSource = list;
+            dgvRecords.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            UniversalStatic.ChangeGridProperties(dgvRecords);
+        }
+
+
+
+        private void DisplayListOutput(string message)
+        {
+            if (dgvRecords.Controls.Count > 2)
+            { dgvRecords.Controls.RemoveAt(2); }
+
+            ShowStatusBar(message, false);
+        }
+
+        private void DisplayEmpty()
+        {
+            ClearGrid();
+            dgvRecords.Controls.Add(new DataEmpty());
+        }
+
+        private void pnlHeader_Paint(object sender, PaintEventArgs e)
+        { UniversalStatic.DrawLineInFooter(pnlHeader, Color.FromArgb(204, 204, 204), 2); }
+
+
+
+        private void btnPowerOff_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            var resultDia = DialogResult.None;
+            resultDia = MessageBox.Show("Do you wish to Power Off the Device ??", "Power Off Device", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (resultDia == DialogResult.Yes)
+            {
+                bool deviceOff = objZkeeper.PowerOffDevice(int.Parse(tbxMachineNumber.Text.Trim()));
+
+            }
+
+            this.Cursor = Cursors.Default;
+        }
+
+        private void btnRestartDevice_Click(object sender, EventArgs e)
+        {
+
+            DialogResult rslt = MessageBox.Show("Do you wish to restart the device now ??", "Restart Device", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (rslt == DialogResult.Yes)
+            {
+                if (objZkeeper.RestartDevice(int.Parse(tbxMachineNumber.Text.Trim())))
+                    ShowStatusBar("The device is being restarted, Please wait...", true);
+                else
+                    ShowStatusBar("Operation failed,please try again", false);
+            }
+
+        }
+
+        private void btnGetDeviceTime_Click(object sender, EventArgs e)
+        {
+            int machineNumber = int.Parse(tbxMachineNumber.Text.Trim());
+            int dwYear = 0;
+            int dwMonth = 0;
+            int dwDay = 0;
+            int dwHour = 0;
+            int dwMinute = 0;
+            int dwSecond = 0;
+
+            bool result = objZkeeper.GetDeviceTime(machineNumber, ref dwYear, ref dwMonth, ref dwDay, ref dwHour, ref dwMinute, ref dwSecond);
+
+            string deviceTime = new DateTime(dwYear, dwMonth, dwDay, dwHour, dwMinute, dwSecond).ToString();
+            List<DeviceTimeInfo> lstDeviceInfo = new List<DeviceTimeInfo>();
+            lstDeviceInfo.Add(new DeviceTimeInfo() { DeviceTime = deviceTime });
+            BindToGridView(lstDeviceInfo);
+        }
+
+
+        private void btnEnableDevice_Click(object sender, EventArgs e)
+        {
+            // This is of no use since i implemented zkemKeeper the other way
+            bool deviceEnabled = objZkeeper.EnableDevice(int.Parse(tbxMachineNumber.Text.Trim()), true);
+
+        }
+
+
+
+        private void btnDisableDevice_Click(object sender, EventArgs e)
+        {
+            // This is of no use since i implemented zkemKeeper the other way
+            bool deviceDisabled = objZkeeper.DisableDeviceWithTimeOut(int.Parse(tbxMachineNumber.Text.Trim()), 3000);
+        }
+
+        private void tbxPort_TextChanged(object sender, EventArgs e)
+        { UniversalStatic.ValidateInteger(tbxPort); }
+
+        private void tbxMachineNumber_TextChanged(object sender, EventArgs e)
+        { UniversalStatic.ValidateInteger(tbxMachineNumber); }
+
+        private void btnUploadUserInfo_Click(object sender, EventArgs e)
+        {
+            // Add you new UserInfo Here and  uncomment the below code
+            //List<UserInfo> lstUserInfo = new List<UserInfo>();
+            //manipulator.UploadFTPTemplate(objZkeeper, int.Parse(tbxMachineNumber.Text.Trim()), lstUserInfo);
+        }
+
+        private void tbxDeviceIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Master_Load(object sender, EventArgs e)
+        {
+            //string connetionString = "server=localhost;database=testdb;uid=root;pwd=\"\";";
+            //string insertQuery = "insert into testdb.test_tbl(name) values ('AAA')";
+
+            //MySqlConnection cnn = new MySqlConnection(connetionString);
+            //MySqlCommand dbCommand = new MySqlCommand(insertQuery, cnn);
+            //MySqlDataReader dbReader;
+            //try
+            //{
+            //    cnn.Open();
+            //    MessageBox.Show("Connection Open ! ");
+            //    dbReader = dbCommand.ExecuteReader();
+            //    cnn.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Can not open connection ! ");
+            //}
+        }
+
+        private void btnSaveData_Click(object sender, EventArgs e)
+        {
+            string connetionString = "server=localhost; database=hris; uid=root; pwd=\"\";";
+            MySqlConnection cnn = new MySqlConnection(connetionString);
+            MySqlCommand dbCommand;
+            MySqlDataReader dbReader;
+            //Expected Date Time format: monthAbbrev/day/year 12hrTime AmPm
+            string queryRecentDate = "SELECT STR_TO_DATE(date_time, \"%b/%d/%Y %r\") AS date_time_record FROM hris.daily_time_record ORDER BY date_time_record DESC LIMIT 1";
+
+            try
+            {
+                cnn.Open();
+                try
+                {
+                    ShowStatusBar(string.Empty, true);
+                    ICollection<MachineInfo> lstMachineInfo = manipulator.GetLogData(objZkeeper, int.Parse(tbxMachineNumber.Text.Trim()));
+
+                    if (lstMachineInfo != null && lstMachineInfo.Count > 0)
+                    {
+                        dbCommand = new MySqlCommand(queryRecentDate, cnn);
+                        dbReader = dbCommand.ExecuteReader();
+                        dbReader.Close();
+                        dbReader.DisposeAsync();
+
+                        object objRecentDate = dbCommand.ExecuteScalar();
+                        string recentDate = objRecentDate != null ? Convert.ToString(dbCommand.ExecuteScalar()) : "Jan/01/2000 1:00:00 am";
+
+                        var recentDbDateTime = DateTime.Parse(recentDate);
+
+                        MessageBox.Show("recent: "+recentDate);
+                        dbReader.Close();
+                        dbReader.DisposeAsync();
+
+
+                        foreach (MachineInfo smpl in lstMachineInfo)
+                        {
+                            if (recentDbDateTime < DateTime.Parse(smpl.DateTimeRecord))
+                            {
+                                string insertQuery = $"insert into hris.daily_time_record(user_id,date_time) values ('{smpl.IndRegID}','{smpl.DateTimeRecord}')";
+                                dbCommand = new MySqlCommand(insertQuery, cnn);
+                                dbReader = dbCommand.ExecuteReader();
+                                dbReader.Close();
+                                dbReader.DisposeAsync();
+                            }
+                        }
+
+                        //BindToGridView(lstMachineInfo);
+                        ShowStatusBar(lstMachineInfo.Count + " records found !!", true);
+
+                        string queryDbRecordCount = "SELECT COUNT(date_time) FROM hris.daily_time_record";
+                        dbCommand = new MySqlCommand(queryDbRecordCount, cnn);
+                        int dbRecordCount = Convert.ToInt32(dbCommand.ExecuteScalar());
+
+                        dbReader.Close();
+                        dbReader.DisposeAsync();
+
+                        if(dbRecordCount == lstMachineInfo.Count)
+                        {
+                            lblDataMatchStatus.Text = "Data Match Status : Both DB Count Matches.";
+                        }
+                        else if(dbRecordCount < lstMachineInfo.Count)
+                        {
+                            lblDataMatchStatus.Text = $"Data Match Status : DB Record Count ({dbRecordCount}) is LESS than Machine DB ({lstMachineInfo.Count}).";
+                        }
+                        else if(dbRecordCount > lstMachineInfo.Count)
+                        {
+                            lblDataMatchStatus.Text = $"Data Match Status : DB Record Count ({dbRecordCount}) is GREATER than Machine DB ({lstMachineInfo.Count}).";
+                        }
+                        else lblDataMatchStatus.Text = "Data Match Status : Unknown! Unexpected comparison.";
+
+                    }
+                    else
+                        DisplayListOutput("No records found");
+                }
+                catch (Exception ex)
+                {
+                    DisplayListOutput(ex.Message);
+                }
+
+
+                
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ShowStatusBar(string.Empty, true);
+
+                ICollection<MachineInfo> lstMachineInfo = manipulator.GetLogData(objZkeeper, int.Parse(tbxMachineNumber.Text.Trim()));
+
+                // Sorting by User ID from machine raw data
+                var orderedInfo = from machineInfo in lstMachineInfo
+                                  orderby machineInfo.IndRegID
+                                  select machineInfo;
+
+                if (lstMachineInfo != null && lstMachineInfo.Count > 0)
+                {
+                    BindToGridView(orderedInfo.ToList());
+                    ShowStatusBar(lstMachineInfo.Count + " records found !!", true);
+                }
+                else
+                    DisplayListOutput("No records found");
+            }
+            catch (Exception ex)
+            {
+                DisplayListOutput(ex.Message);
+            }
+        }
+    }
+}
